236ac6d6f075f50240da60516a809cc43652749d
luiz-developer-7 <116402019+luiz-developer-7@users.noreply.github.com>
2022-10-26 15:52:49 -0300

    v3.5

--- quiz/js/quiz-js.js
+++ quiz/js/quiz-js.js
@@ -3,3 +3,12 @@ document.getElementById('q2').style.display = "none";
 document.getElementById('q3').style.display = "none";
 document.getElementById('q4').style.display = "none";
 document.getElementById('q5').style.display = "none";
+
+
+
+document.getElementById("btn-comecar").onclick = function() {start()};
+
+function start() {
+    document.getElementById('inicio').style.display = "none"; 
+    document.getElementById('q1').style.display = "block"; 
+}
