a9578d4b8ba81d11be876389e56ec483f347b334
luiz-developer-7 <116402019+luiz-developer-7@users.noreply.github.com>
2022-10-26 15:41:24 -0300

    v3.1

--- quiz/js/quiz-js.js
+++ quiz/js/quiz-js.js
@@ -1,1 +1,5 @@
 document.getElementById('q1').style.display = "none";
+document.getElementById('q2').style.display = "none";
+document.getElementById('q3').style.display = "none";
+document.getElementById('q4').style.display = "none";
+document.getElementById('q5').style.display = "none";
